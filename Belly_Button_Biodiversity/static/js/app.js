89f472e34f8d8f0a107e34070a2c34e7e0f4adf5
Ted Smiley Jr <theodore.smileyjr@gmail.com>
2018-11-17 12:44:33 -0500

    update app.js code

--- Belly_Button_Biodiversity/static/js/app.js
+++ Belly_Button_Biodiversity/static/js/app.js
@@ -1,97 +1,90 @@
-function buildMetadata(sample) {
+ function buildMetadata(sample) {
 
-  // @TODO: Complete the following function that builds the metadata panel
+// @TODO: Complete the following function that builds the metadata panel
+console.log("Is this being run at all?");
+// Use `d3.json` to fetch the metadata for a sample
+d3.json(`/metadata/${sample}`).then(function(metadata, error)  {
+  console.log(metadata);
+  if (error) return console.warn(error);
+  var $metadata = document.getElementById("sample-metadata");
+  $metadata.innerHTML = "";
+  for (var key in metadata){
+    console.log("We are in the loop");
+    tag = document.createElement("h6");
+    h6text=document.createTextNode(`${key}:${metadata[key]}`);
+    tag.append(h6text);
+    $metadata.appendChild(tag);
+  }
 
-  // Use `d3.json` to fetch the metadata for a sample
-    // Use d3 to select the panel with id of `#sample-metadata`
-var dataset =[]
-d3.csv("dataSets/belly_button_metadata.csv", function(D) {
-  return{
-    ctu_id: +d.ctu_id,
-    ctu_labelP d.ctu_label, // need to iterate thru columns
-    data: 
-  };
-  , fuction(data){
-    console.log(d[0]);
-  };
 });
-    // Use `.html("") to clear any existing metadata
+//     // Use d3 to select the panel with id of `#sample-metadata`
 
-    // Use `Object.entries` to add each key and value pair to the panel
-    // Hint: Inside the loop, you will need to use d3 to append new
-    // tags for each key-value in the metadata.
+//     // Use `.html("") to clear any existing metadata
 
-    // BONUS: Build the Gauge Chart
-    // buildGauge(data.WFREQ);
-    // 
-    
-    
-    
-    
-    // // Part 5 - Working Pie Chart
-    // 
-    // Columns in the csv
-    // EVENT,ETHNICITY,GENDER,AGE,WFREQ,BBTYPE,LOCATION,COUNTRY012,ZIP012,COUNTRY1319,ZIP1319,
-    // DOG,CAT,IMPSURFACE013,NPP013,MMAXTEMP013,PFC013,IMPSURFACE1319,NPP1319,MMAXTEMP1319,PFC1319
-    // 
-    // html:
-    // // <script>
-    //     var url = "/pie";
-    //     d3.json(url).then(function(data){
-    //         var layout = {
-    //             title: "Lyric Frequency"}
-    //         Plotly.plot("pie", data, layout);
-    //     });
-    //     </script>
-    // 
-    // Day 2 Act. 4 Stu_Stock shows 
-    // How to Grab values from the data json object to build the plots
+//     // Use `Object.entries` to add each key and value pair to the panel
+//     // Hint: Inside the loop, you will need to use d3 to append new
+//     // tags for each key-value in the metadata.
 
-var trace1 = {
-  labels: [otu_ids],
-  values: [sample_values],
-  type: 'pie'
-};
-// Trace1 for the Greek Data
-var trace1 = {
-  x: data.map(row => row.greekSearchResults),
-  y: data.map(row => row.greekName),
-  text: data.map(row => row.greekName),
-  name: "Greek",
-  type: "pie"
-};
-
-var data = [trace1];
-
-var layout = {
-  title: "'Pie' Chart",
-};
-
-Plotly.newPlot("pie", data, layout);
-
-}
-var dataset =[]
-d3.csv("dataSets/belly_button_data.csv", function(D) {
-  return{
-    ctu_id: +d.ctu_id,
-    ctu_labelP d.ctu_label,
-    data: 
-  };
-  , fuction(data){
-    console.log(d[0]);
-  };
-});
+//     // BONUS: Build the Gauge Chart
+//     // buildGauge(data.WFREQ);
+ }
 
 function buildCharts(sample) {
+  
+  console.log("what is sample"+sample);
+  d3.json(`/samples/${sample}`).then(function(sdata, error)  {
+    console.log(sdata);
+    if (error) return console.warn(error);
+    var $metadata = document.getElementById("sample-metadata");
+    $metadata.innerHTML = "";
 
-  // @TODO: Use `d3.json` to fetch the sample data for the plots
+    // Pie Chart
+    var piedata=[
+      {
+        values:sdata["sample_values"].slice(0,10),
+        labels:sdata["otu_ids"].slice(0,10),
+        hovertext:sdata["otu_labels"].slice(0,10),
+        hoverinfo:"hovertext",
+        type:"pie"
+      }
+    ];
+    var pielayout ={
+      margin: {t:0, l:0}
+    };
+// purge will be used on the div that you wish clear of Plotly plots
+    Plotly.purge("pie");
+    Plotly.plot("pie",piedata,pielayout)
+    
 
-    // @TODO: Build a Bubble Chart using the sample data
+// Bubble Chart
+    var bubbledata =[
+      {
+      "x": sdata.otu_ids,
+      "y": sdata.sample_values,
+      "text": sdata.otu_labels,
+      "mode": "markers",
+      "marker": {
+          "size": sdata.sample_values,
+          "color": sdata.otu_ids,
+          "colorscale": "Viridis"
+        }
+      }
+    ];
+    Plotly.purge("bubble");
+    Plotly.plot("bubble",bubbledata)
+  });
+   
+// purge will be used on the div that you wish clear of Plotly plots
 
-    // @TODO: Build a Pie Chart
-    // HINT: You will need to use slice() to grab the top 10 sample_values,
-    // otu_ids, and labels (10 each).
-}
+    
+};
+//   // @TODO: Use `d3.json` to fetch the sample data for the plots
+
+//     // @TODO: Build a Bubble Chart using the sample data
+
+//     // @TODO: Build a Pie Chart
+//     // HINT: You will need to use slice() to grab the top 10 sample_values,
+//     // otu_ids, and labels (10 each).}
 
 function init() {
   // Grab a reference to the dropdown select element
@@ -113,11 +106,11 @@ function init() {
   });
 }
 
-function optionChanged(newSample) {
-  // Fetch new data each time a new sample is selected
-  buildCharts(newSample);
-  buildMetadata(newSample);
-}
+ function optionChanged(newSample) {
+   // Fetch new data each time a new sample is selected
+   buildCharts(newSample);
+   buildMetadata(newSample);
+ }
 
 // Initialize the dashboard
 init();
